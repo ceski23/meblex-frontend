2701bd2e68c5a4f23e00683afedd7ffdaee025f9
Cezary Bober <ceski23@gmail.com>
2019-05-01 16:02:43 +0200

    Add updating user data

--- src/components/userProfile/UserProfile.js
+++ src/components/userProfile/UserProfile.js
@@ -2,16 +2,29 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { SubmissionError } from 'redux-form';
 import UserProfileForm from './UserProfileForm';
+import * as API from '../../api';
 
 import S from './UserProfile.module.scss';
 
 const UserProfile = () => {
   const user = useSelector(state => state.auth.user);
 
-  const updateUserProfile = (values) => {
-    throw new SubmissionError({
-      _error: 'Jeszce nie jest gotowe',
-    });
+  const updateUserProfile = async (values) => {
+    // setIsLoading(true);
+    try {
+      await API.updateUserData(values);
+    } catch (error) {
+      Object.keys(error.errors).forEach((key) => {
+        error.errors[key.toLowerCase()] = String(error.errors[key]);
+        delete error.errors[key];
+      });
+
+      throw new SubmissionError({
+        _error: error.title,
+        ...error.errors,
+      });
+    }
+    // setIsloading(false);
   };
 
   return (
