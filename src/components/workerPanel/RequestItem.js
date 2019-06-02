5037a9a5da0c585315674f1ff5abacfff9f3c36e
Cezary Bober <ceski23@gmail.com>
2019-06-02 12:46:52 +0200

    Fix UI size

--- src/components/workerPanel/RequestItem.js
+++ src/components/workerPanel/RequestItem.js
@@ -37,8 +37,8 @@ const RequestItem = ({ request, requestAccepted, ...props }) => {
     `,
 
     image: css`
-      width: 70px;
-      height: 70px;
+      width: 60px;
+      height: 60px;
       margin: 0 20px;
       display: flex;
       justify-content: center;
@@ -48,7 +48,7 @@ const RequestItem = ({ request, requestAccepted, ...props }) => {
     fallbackIcon: css`
       width: 100%;
       height: 100%;
-      margin: 0 15px;
+      margin: 0 10px;
       fill: ${theme.colors.text};
     `,
 
@@ -59,17 +59,12 @@ const RequestItem = ({ request, requestAccepted, ...props }) => {
 
     text: css`
       margin: 0;
-      font-size: 1.1em;
+      font-size: 0.9em;
     `,
 
     size: css`
-      margin: 12px 0 0 0;
-      font-size: 0.9em;
-
-      span {
-        color: ${theme.colors.text};
-        font-size: .8em;
-      }
+      margin: 5px 0 0 0;
+      font-size: 0.8em;
     `,
 
     field: css`
@@ -96,6 +91,11 @@ const RequestItem = ({ request, requestAccepted, ...props }) => {
       font-size: .9em;
       height: 42px;
       line-height: 42px;
+    `,
+
+    cos: css`
+      margin: 5px 10px 0 0;
+      font-size: 0.8em;
     `,
 
     data: css`
@@ -142,7 +142,7 @@ const RequestItem = ({ request, requestAccepted, ...props }) => {
         <div css={style.textBox}>
           <h4 css={style.text}>{request.pieceOfFurniture.name}</h4>
           <div css={{ display: 'flex', flexDirection: 'row', marginRight: 10 }}>
-            <p css={{ margin: '10px 10px 0 0' }}>Rozmiar:</p>
+            <p css={style.cos}>Rozmiar:</p>
             <h3 css={style.size}>{request.pieceOfFurniture.size.split('x').join(' x ')}</h3>
           </div>
         </div>
